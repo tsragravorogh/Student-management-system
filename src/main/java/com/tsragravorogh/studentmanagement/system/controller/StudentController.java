05aa3afa7ea6f9733f05a9ab4adb5272f5fdbfed
tsragravorogh <71145107+tsragravorogh@users.noreply.github.com>
2021-08-04 11:57:34 +0300

    "Students" and "create student" pages were created

--- src/main/java/com/tsragravorogh/studentmanagement/system/controller/StudentController.java
+++ src/main/java/com/tsragravorogh/studentmanagement/system/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.tsragravorogh.studentmanagement.system.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -26,6 +28,13 @@ public class StudentController {
     public String createStudentForm(Model model){
         // create student object to hold from data
         Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    }
 
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 }
