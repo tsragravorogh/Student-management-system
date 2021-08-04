15bb7c3c27b89d5e6880b75404b1801025513a05
tsragravorogh <71145107+tsragravorogh@users.noreply.github.com>
2021-08-04 14:20:12 +0300

    Update and delete functions were created

--- src/main/java/com/tsragravorogh/studentmanagement/system/service/StudentService.java
+++ src/main/java/com/tsragravorogh/studentmanagement/system/service/StudentService.java
@@ -8,4 +8,10 @@ public interface StudentService {
     List<Student> getAllStudent();
 
     Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
 }
